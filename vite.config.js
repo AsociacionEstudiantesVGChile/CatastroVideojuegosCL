88c855e7663ce48413a2cdd2d9d24cbc8410c92f
Alfonsina <alfonsina.l@gmail.com>
2025-08-01 14:58:24 -0400

    add gh-pages configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/CatastroVideojuegosCL",
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "dashes",
     },
   },
-  envPrefix: 'VITE_',
+  envPrefix: "VITE_",
 });
